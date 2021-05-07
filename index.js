1e9d283911dbccd9a7b0e10c5522c60646a4ad5d
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-05-06 23:34:16 -0700

    declare topMovies

--- index.js
+++ index.js
@@ -11,3 +11,59 @@ app.use((err, req, res, next) => {
     console.log(err.stack);
     res.status(500).send('Something broke!');
 });
+
+// Define topMovies 
+
+let topMovies = [
+    {
+        title: 'The Shawshank Redemption',
+        director: 'Frank Darabont',
+        year: '1994'
+    },
+    {
+        title: 'Inception',
+        director: 'Christopher Nolan',
+        year: '2010'
+    },
+    {
+        title: 'The Dark Knight',
+        director: 'Christopher Nolan',
+        year: '2008'
+    },
+    {
+        title: 'Avengers: Infinity War',
+        director: 'Anthony Russo, Joe Russo',
+        year: '2018'
+    },
+    {
+        title: 'The Wolf of Wall Street',
+        director: 'Martin Scorsese',
+        year: '2013'
+    },
+    {
+        title: 'Pulp Fiction',
+        director: 'Quentin Tarantino',
+        year: '1963'
+    },
+    {
+        title: 'Fight Club',
+        director: 'David Fincher',
+        year: '1999'
+    },
+    {
+        title: 'Parasite',
+        director: 'Bong Joon Ho',
+        year: '2019'
+    },
+    {
+        title: 'The Lion King',
+        director: 'Roger Allers, Rob Minkoff',
+        year: '1994'
+    },
+    {
+        title: 'Alien',
+        director: 'Ridley Scott',
+        year: '1979'
+    }
+];
+
