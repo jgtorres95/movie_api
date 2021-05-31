1161569b2bddf171e4d431ee347bafd16b16d2dc
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-05-30 19:50:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -26,17 +26,7 @@ require('./passport');
 
 const cors = require('cors');
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-
-            let message = 'The CORS policy for this application does not allow access from this origin ' + origin;
-            return callback(new Error(message ), false);
-        }
-        return callback(null,true);
-    }
-}));
+app.use(cors());
 
 const { check, validationResult } = require('express-validator');
 
