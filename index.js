809923be229276ff96e8a5259154613639723dbe
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-10-19 20:59:31 -0700

    add authentication to get request

--- index.js
+++ index.js
@@ -80,14 +80,14 @@ app.get('/movies/directors/:Name', passport.authenticate('jwt', { session: false
 });
 
 // Return data about a user by username
-app.get('/users/:Username', (req, res) => {
+app.get('/users/:Username', passport.authenticate('jwt', { session: false}), (req, res) => {
   Users.findOne({ Username : req.params.Username })
     .then((user) => {
       res.json(user);
     })
     .catch(err => {
       console.error(err);
-      res.status(500).send('Error: ' + err);
+      res.status(500).send('Error: ' + err);  
     })
 });
 
