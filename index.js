15dafdd1fa632d00eeb16c7b406f64d7d15b8731
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-11-09 19:38:25 -0800

    allow access to API for a set of origins

--- index.js
+++ index.js
@@ -25,8 +25,18 @@ app.use(morgan('common'));
 // run passport file
 require('./passport');
 
-// enable CORS
-app.use(cors());
+// create allowedOrigins array and enable CORS
+let allowedOrigins = ['http://localhost:8080', 'https://jgtorres95-myflix-react.netlify.app/', 'https://jgtorres95.github.io/myFlix-Angular-app/'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // implement authorization
 require('./auth')(app);
